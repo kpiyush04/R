354524a6e78484c16add4da3cb394685e887b173
Piyush Kumar <pkumar22122001@gmail.com>
2020-07-14 00:18:02 +0530

    basic of R

---
+++ very basics/Dice.R
@@ -0,0 +1,11 @@
+library("ggplot2")
+
+
+roll <- function(){
+  die <- 1:6
+  dice <- sample(die, size = 2, replace = TRUE,  prob = c(1/8, 1/8, 1/8, 1/8, 1/8, 3/8))
+  sum(dice)
+}
+
+rolls <- replicate(10000, roll())
+qplot(rolls, binwidth = 1)
